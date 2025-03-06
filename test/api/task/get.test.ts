dfe5481fa843c9323b2a3ce39901615c45a41747
Gabriel Marques Magalhães <gabriel.magalhaes.dev@gmail.com>
2025-03-06 14:05:04 -0300

    improve task get test

--- test/api/task/get.test.ts
+++ test/api/task/get.test.ts
@@ -7,34 +7,31 @@ import {
 
 import { AppModule } from "@infrastructure/server/modules/app.module";
 
-describe("Cats", () => {
-  let app: NestFastifyApplication;
+let app: NestFastifyApplication;
 
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule]
-    }).compile();
+beforeAll(async () => {
+  const moduleRef = await Test.createTestingModule({
+    imports: [AppModule]
+  }).compile();
 
-    app = moduleRef.createNestApplication<NestFastifyApplication>(
-      new FastifyAdapter()
-    );
-    await app.init();
-    await app.getHttpAdapter().getInstance().ready();
-  });
+  app = moduleRef.createNestApplication<NestFastifyApplication>(
+    new FastifyAdapter()
+  );
+  await app.init();
+  await app.getHttpAdapter().getInstance().ready();
+});
 
-  it(`/GET cats`, () => {
-    return app
-      .inject({
-        method: "GET",
-        url: "/task"
-      })
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-        console.log(response.payload);
-      });
-  });
+afterAll(async () => {
+  await app.close();
+});
 
-  afterAll(async () => {
-    await app.close();
+describe("GET /task", () => {
+  it(`Getting tasks`, async () => {
+    const response = await app.inject({
+      method: "GET",
+      url: "/task"
+    });
+    expect(response.statusCode).toBe(200);
+    console.log(response.payload);
   });
 });
